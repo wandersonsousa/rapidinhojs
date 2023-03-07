a61656d6418c2f50377d8c62be7a913a5d0481ce
Wanderson <wandersonsousa3004@gmail.com>
2023-03-07 11:53:11 -0300

    chore: babel config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import SimpleReact from "./SimpleReact.js";
+import RapidinhoJs from "./Rapidinho.js";
 
 const $root = document.getElementById("root");
 
-SimpleReact.render(
-  <div id="name">
-    <h1>Hello, world with Babel!</h1>
-    <div style="border:1px solid #3498db;background:#ecf0f1;">
-      <p>This is a paragraph</p>
-    </div>
-    <h2 style="color:#e74c3c;"></h2>
+RapidinhoJs.render(
+  <div
+    id="app-container"
+    style="background:#ecf0f1;padding:64px;font-family:Arial;height:100%;margin-top:auto;margin-bottom:auto;"
+  >
+    <h1 style="color:#2ecc71;text-align:center;">Quick development with babel!</h1>
+    <p style="color:#2ecc71;text-align:center;">
+      This is a simple example of how to use the <code>Rapidinho</code> library.
+    </p>
   </div>,
   $root
 );
